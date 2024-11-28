b74b2e2e8f7ebe06f662179cd732b354d3b108bc
h3xit <h3xit@protonmail.com>
2024-11-28 10:31:12 -0500

    fix docker build script

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
 python -m hatchling build && \
-docker build -t ghcr.io/h3xitsec/h3xrecon/client:dev -f ./Dockerfile .
+# Extract the VERSION from __about__.py using grep with Perl regex
+VERSION=$(grep -oP '(?<=__version__ = ").*(?=")' ./src/h3xrecon_client/__about__.py)
+
+# Check if VERSION extraction was successful
+if [[ -z "$VERSION" ]]; then
+    echo "Failed to extract VERSION. Exiting."
+    exit 1
+fi
+
+echo "Building client version ${VERSION}"
+
+docker build -t ghcr.io/h3xitsec/h3xrecon/client:${VERSION} -f ./Dockerfile .
